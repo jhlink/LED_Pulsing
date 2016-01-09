73270059639411276d106a525817c11677b5b4b4
jhlink <jamesmankim@gmail.com>
2016-01-08 21:46:39 -0500

    Init file for LED pulsing

---
+++ LED/LED.ino
@@ -0,0 +1,79 @@
+
+#include <Adafruit_NeoPixel.h>
+
+#define PIN             6
+#define NUMPIXELS       1
+#define OSCIL_VAL       450
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+//  Determines how smooth the fade in/fade out behaviour is
+int delayval = 25;
+
+//  A stable, consistent incremental value is required. 
+//      millis() is too unstable, and therefore difficult to control
+//      fade in/out length
+long commIncVal = 0;
+
+void setup() {
+    Serial.begin(9600); 
+    pixels.begin(); 
+    pixels.setPixelColor(0, pixels.Color(240,255,0));
+}
+
+void loop() {
+    //  The following initializes the oscillating value, which will be
+    //      anywhere from 0 to OSCIL_VAL/2
+    uint8_t i = oscilPulse(commIncVal++); 
+    
+    //  This loop is where the color intensities vary. The shorter the delay
+    //      the smoother the transitions. Changing the incrementation value
+    //      will also change smoothness.
+    //
+    //  The loop only evaluates given that the interval is within OSCIL_VAL and 5.
+    while(i != OSCIL_VAL && i >= 5) {
+        i = oscilPulse(commIncVal+=3); 
+
+        //  Vary brightness of LED at whatever color.
+        pixels.setBrightness(i);
+
+        //  Whenever changes are made to the pixel color, the show method must
+        //      follow in order to apply color changes.  
+        pixels.show();                      
+
+        //  The lower the delay (ms), the smoother the appearance. 
+        delay(delayval);            
+    }
+
+    //  This will hold the current LED brightness/intensity at the end of every loop,
+    //      signifiying the end of a light pattern as described above. 
+//  delay(10);
+}
+
+//  This is the mathematical function that allows pulsing concurrently with any
+//      existing processes that are being executed. 
+uint8_t oscilPulse(long incVal) {
+    //  Oscillation is divided into three parts.
+
+    //  This is the incremental range and increasing direction from 0 to OSCIL_VAL. 
+    int incFun = incVal % OSCIL_VAL;
+
+    //  This evaluates to be either 0 or 1 given incFun is 0 when less than
+    //      OSCIL_VAL/2 and 1 when higher than OSCIL_VAL/2.
+    //  This allows selective subtraction when whenToSub = 0 or 1.
+    int whenToSub = incFun / (OSCIL_VAL / 2);
+
+    //  This is the range to subtract given incFun, which is (0 - (OSCIL_VAL/2)) to
+    //      (OSCIL_VAL - (OSCIL_VAL/2)) -> -(OSCIL_VAL/2) to OSCIL_VAL/3.
+    //  Ultimately, this is what allows an incremental decrease upon reaching
+    //      OSCIL_VAL.
+    int decFun = ((incVal % OSCIL_VAL) - (OSCIL_VAL / 2));
+
+    //  The final formula is as follows, where upon subtraction is allowed, 
+    //      the range is doubled to offset the range [OSCIL_VAL/2, OSCIL_VAL]
+    //      to allow a decreasing behaviour.
+    int result = incFun - (whenToSub * decFun * 2);
+
+    //  The final result.
+    return (uint8_t) result;
+}
